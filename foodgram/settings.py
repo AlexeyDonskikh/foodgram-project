ba06cf31dd12f2ac168e28fec0d4883d7da756cb
AlexeyDonskikh <donskikhalexey@gmail.com>
2021-02-21 22:22:41 +0300

    Deploy

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a^u+2+-3*au3j9^y@qf^)vgq0(o_xe4bhq8osbk*dk(#hi-=(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
