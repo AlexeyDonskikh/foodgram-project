8ec6f6d4e23fff80e2bad8a9421887904e52f31d
AlexeyDonskikh <donskikhalexey@gmail.com>
2021-03-16 21:25:08 +0300

    pass reset

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -151,6 +151,8 @@ REST_FRAMEWORK = {
     )
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
